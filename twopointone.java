93fdd2e85e60cefbe4e580c247ec8cea6d9ab195
manas-ku <150448059+manas-ku@users.noreply.github.com>
2026-05-18 08:33:48 +0000

    initial commit

---
+++ twopointone.java
@@ -0,0 +1,12 @@
+public class twopointone {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        double annualPay;
+        name="Manas";
+        age = 22;
+        annualPay = 2306784.00;
+        System.out.println("My name is " + name + ", my age is " + age + " and");
+        System.out.println("I hope to earn Rs" + annualPay + " per year.");
+    }
+}
